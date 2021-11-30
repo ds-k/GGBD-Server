701b94fbdd0a724c65882287be4094e8db059aab
daeseongkim05 <daeseongkim05@gmail.com>
2021-12-01 04:21:42 +0900

    FIX *_hospitial route

--- routes/hospital.js
+++ routes/hospital.js
@@ -2,9 +2,6 @@ const router = require("express").Router();
 
 const { get } = require("../controllers/hospital");
 
-router.get("/", get); // * 유저 정보
-// router.patch("/", update); // *  유저 정보 수정
-// router.post("/login", login); // * 유저 로그인
-// router.delete("/logout", logout); // * 유저 로그아웃
+router.get("/", get); // * 병원 정보
 
 module.exports = router;
