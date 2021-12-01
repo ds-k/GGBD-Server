3046277accb2fd2ec28cd80a0305ccace6122f22
daeseongkim05 <daeseongkim05@gmail.com>
2021-12-02 03:18:18 +0900

    CREATE department_get, detail endpoint

--- controllers/department/get.js
+++ controllers/department/get.js
@@ -1,3 +1,11 @@
-module.exports = (req, res) => {
-  res.send("departments-get");
+const { departments } = require("../../models");
+
+module.exports = async (_, res) => {
+  try {
+    const getData = await departments.findAll();
+    const data = getData.map((el) => el.get({ plain: true }).name);
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(404).json({ message: e.message });
+  }
 };
