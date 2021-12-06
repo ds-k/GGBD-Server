28b794d6ef9d1dae1d92268ed2523d984cbc9583
daeseongkim05 <daeseongkim05@gmail.com>
2021-12-06 23:24:56 +0900

    CREATE Auth_google, kakao router

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
-const { login, get, logout, update } = require("../controllers/user");
+const {
+  googleLogin,
+  kakaoLogin,
+  get,
+  logout,
+  update,
+} = require("../controllers/user");
 
 router.get("/", get); // * 유저 정보
 router.patch("/", update); // *  유저 정보 수정
-router.post("/login", login); // * 유저 로그인
+router.post("/login/kakao", kakaoLogin); // * 유저 카카오 로그인
+router.post("/login/kakao", googleLogin); // * 유저 구글로그인
 router.delete("/logout", logout); // * 유저 로그아웃
 
 module.exports = router;
