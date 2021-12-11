d9b52141faad1cfe6b310d87214e90b315cae882
devSominPark <min85472@gmail.com>
2021-12-12 03:33:13 +0900

    CREATE Hospital_get endpoint (all)

--- controllers/hospital/get.js
+++ controllers/hospital/get.js
@@ -1,3 +1,13 @@
-module.exports = (req, res) => {
-  res.send("hospital-get");
+const { hospitals } = require("../../models");
+
+module.exports = async (_, res) => {
+  try {
+    const getData = await hospitals.findAll({
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    const data = getData.map((el) => el.get({ plain: true }));
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(404).json({ message: e.message });
+  }
 };
