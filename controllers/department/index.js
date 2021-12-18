d92ee38bc6afe5646f19e6c9ebfc85aa6b5ffb33
devSominPark <min85472@gmail.com>
2021-12-18 17:31:41 +0900

    CREATE Reply_department reply endpoint

--- controllers/department/index.js
+++ controllers/department/index.js
@@ -2,10 +2,14 @@ const get = require("./get");
 const detail = require("./detail");
 const reply = require("./reply");
 const postReply = require("./postReply");
+const remove = require("./remove");
+const patch = require("./patch");
 
 module.exports = {
   get,
   detail,
   reply,
   postReply,
+  remove,
+  patch,
 };
