1374b597aca3f3ed631322d58fd269ee709cb853
devSominPark <min85472@gmail.com>
2021-12-19 21:59:52 +0900

    CREATE Detail_like & scrap endpoint

--- routes/post.js
+++ routes/post.js
@@ -8,14 +8,22 @@ const {
   remove,
   reply,
   main,
+  postLike,
+  deleteLike,
+  postScrap,
+  deleteScrap,
 } = require("../controllers/post");
 
 router.get("/:department", get); // * 글 정보 전체 가져오기
 router.get("/detail/:slug", detail); // * 글 정보 개별 가져오기
-router.post("/", post); // * 글 정보 생성하기
+router.post("/post", post); // * 글 생성하기
 router.patch("/", update); // * 글 수정하기
 router.delete("/", remove); // * 글 삭제하기
 router.get("/reply/:slug", reply); // * 글 댓글 불러오기
 router.get("/main/:order", main);
+router.post("/postLike", postLike); // * 응원하기
+router.delete("/deleteLike", deleteLike); // * 응원 취소하기
+router.post("/postScrap", postScrap); // * 스크랩하기
+router.delete("/deleteScrap", deleteScrap); // * 스크랩 취소하기
 
 module.exports = router;
